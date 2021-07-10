51ef3b36dfcacc6bcc7a1321d2e83fdc2ed1381f
Rydenko Denys <d.rudenko@expoplatform.com>
2021-07-10 13:17:15 +0300

    10 03 2021 lesson

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect, useReducer, useCallback, useMemo } from 'react'
 import './App.css';
-import ClassComponent from './ClassComponent'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const handlePulsCount = () => {
-    setCount(count + 1)
+function reducer(state, action) {
+  switch (action.type) {
+    case 'plus':
+      return { count: state.count + 1 };
+    case 'minus':
+      return { count: state.count - 1 }
+    default:
+      return state.count
   }
+}
 
-  const showComponents = () => {
-    if (count === 0) {
-      return <ClassComponent count={count} setCount={handlePulsCount} title='John weak' />
-    } else {
-      return <ClassComponent title='John Dou' />
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  const [countReducer, dispatch] = useReducer(reducer, { count: 0 })
+
+  useEffect(() => {
+    //componentDidMount
+    //componentWillUpdate
+    console.log('did mount')
+    console.log('did update')
+    return () => {
+      //componentWillUnmount
+      console.log('unmount')
     }
+  }, [count])
+
+  useEffect(() => {
+    console.log('some')
+  }, [])
+
+  const handleChangeCount = useCallback(
+    () => {
+      setCount(count + 1)
+    },
+    [count],
+  )
+
+
+  const handleChangeDispatch = () => {
+    dispatch({ type: 'plus' })
   }
   return (
     <div className="App">
-      <h1>John Weak</h1>
-      <button onClick={handlePulsCount}>Click</button>
-      {count === 0 ?
-        <ClassComponent count={count} setCount={handlePulsCount} title='John weak' /> :
-        <ClassComponent title='John Dou' />
-      }
+      <p>Hello world</p>
+      <button onClick={handleChangeCount}>Click</button>
+      <button onClick={handleChangeDispatch}>Click1</button>
     </div>
   );
 }
